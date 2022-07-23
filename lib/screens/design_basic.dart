90852b86c1235fc8cfe5f3352636d349995f4c10
Cristhian <criskp007@gmail.com>
2022-07-22 22:26:24 -0400

    create screen home

--- lib/screens/design_basic.dart
+++ lib/screens/design_basic.dart
@@ -11,6 +11,7 @@ class BasicScreen extends StatelessWidget {
         scrollDirection: Axis.vertical,
         children: [
           Page1(),
+          Page2(),
         ],
       ),
     );
@@ -23,9 +24,31 @@ class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: [
-        background(),
-      ],
+      children: [Background(), ColumnContent()],
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xff30BAD6),
+      child: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.pushNamed(context, 'design_basic');
+          },
+          child: Text(
+            'Bienvenido',
+            style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
+          style: TextButton.styleFrom(
+              backgroundColor: Color(0xff0098FA), shape: StadiumBorder()),
+        ),
+      ),
     );
   }
 }
@@ -35,6 +58,39 @@ class Background extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Color(0xff30BAD6),
+      height: double.infinity,
+      alignment: Alignment.topCenter,
+      child: Image(
+        image: AssetImage('assets/fondo.png'),
+      ),
+    );
+  }
+}
+
+class ColumnContent extends StatelessWidget {
+  const ColumnContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var textStyle = TextStyle(
+        fontSize: 60, fontWeight: FontWeight.bold, color: Colors.white);
+    return SafeArea(
+      bottom: false,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            '23°',
+            style: textStyle,
+          ),
+          Text('Lunes', style: textStyle),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, size: 100, color: Colors.white)
+        ],
+      ),
+    );
   }
 }

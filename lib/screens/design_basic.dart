19d22ff198100ad9eaa2a1ffe825b219c005dd82
Cristhian <criskp007@gmail.com>
2022-07-22 20:59:29 -0400

    first commit

---
+++ lib/screens/design_basic.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class BasicScreen extends StatelessWidget {
+  const BasicScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        children: [
+          Page1(),
+        ],
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        background(),
+      ],
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
